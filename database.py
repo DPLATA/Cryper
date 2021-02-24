4185f145501c2093d87fe30fe0af167a8bdc2802
Diego G Plata <diegogplata@gmail.com>
2021-02-24 08:48:41 -0600

    ♻️refactor database file

--- database.py
+++ database.py
@@ -2,12 +2,17 @@ import pandas as pd
 import requests
 import sqlalchemy as sql
 
-response = requests.get('http://api.bitso.com/v3/trades/?book=btc_mxn')
+BITSO_API_URL = 'http://api.bitso.com/v3/trades/?book=btc_mxn'
+MYSQL_LOCALHOST_URL = 'mysql+mysqldb://root:AsdfG95!@localhost/bitso_api'
+
+# DATAFRAME CREATION FROM BITSO API DATA
+
+response = requests.get(BITSO_API_URL)
 json_response = response.json()
 data = json_response['payload']
 df = pd.DataFrame(data)
 
-engine = sql.create_engine('mysql+mysqldb://root:AsdfG95!@localhost/bitso_api')
+# QUERY VALUES
 
 initial_q = 'INSERT INTO bitso_trades (book,created_at,amount,maker_side,price,tid) VALUES '
 
@@ -22,5 +27,15 @@ values_q = ','.join(['(\'{}\',\'{}\',{},\'{}\',{},{})'.format(
 
 end_q = ' ON DUPLICATE KEY UPDATE book = values(book), created_at = values(created_at), amount = values(amount), maker_side = values(maker_side), price = values(price), tid = values(tid);'
 
-query = initial_q + values_q + end_q
+def create_db_engine(url):
+    engine = sql.create_engine(url)
+    return engine
+
+def create_query(i, v, e):
+    query = i + v + e
+    return query
+
+engine = create_db_engine(MYSQL_LOCALHOST_URL)
+query = create_query(initial_q, values_q, end_q)
+
 engine.execute(query)
