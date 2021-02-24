70c09ebda4e60864953f3b0234078f43a21a5ce5
Diego G Plata <diegogplata@gmail.com>
2021-02-23 21:56:15 -0600

    ➕add dependencies🔨update database script🗃database filler automation almost ready

--- database.py
+++ database.py
@@ -4,6 +4,24 @@ import sqlalchemy as sql
 
 response = requests.get('http://api.bitso.com/v3/trades/?book=btc_mxn')
 json_response = response.json()
-data = json_response["payload"]
+data = json_response['payload']
 df = pd.DataFrame(data)
 print(df.columns)
+
+engine = sql.create_engine('mysql+mysqldb://root:AsdfG95!@localhost/bitso_api')
+
+initial_q = 'INSERT INTO bitso_trades (book,created_at,amount,maker_side,price,tid) VALUES '
+
+values_q = ','.join(['({},{},{},{},{},{})'.format(
+    row.book,
+    row.created_at,
+    row.amount,
+    row.maker_side,
+    row.price,
+    row.tid) for idx, row in df.iterrows()
+])
+
+end_q = ' ON DUPLICATE KEY UPDATE book = values(book), created_at = values(created_at), amount = values(amount), maker_side = values(maker_side), price = values(price), tid = values(tid);'
+
+query = initial_q + values_q + end_q
+print(query)
